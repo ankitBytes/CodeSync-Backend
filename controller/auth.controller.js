29a0e33876e1cb1846d1c398ccc312a913405a11
ankitBytes <sahuankitkumar99@gmail.com>
2025-10-19 09:28:30 +0530

    changes in the .env file and frontend url set

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -65,7 +65,7 @@ export const googleCallback = (req, res, next) => {
             maxAge: 24 * 60 * 60 * 1000, // 24 hours
           });
 
-          const redirectUrl = process.env.FRONTEND_URL || "http://localhost:5173/";
+          const redirectUrl = process.env.CLIENT_URL || "http://localhost:5173/";
           return res.redirect(redirectUrl);
         } catch (innerErr) {
           console.error("Token Generation Error:", innerErr.message);
